d8ff98917492d07ef438dbf50465c271e33a8b94
sachin <sachin2sharma003@gmail.com>
2023-07-24 03:29:28 +0530

    sachin-3

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -119,8 +119,11 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
+  window = {
+
   documentation = {
     border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  },
   },
   experimental = {
     ghost_text = false,
