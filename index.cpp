6ce26b8624555224c5b91bec771f64aee6309173
sachin <sachin2sharma003@gmail.com>
2023-07-25 18:39:41 +0530

    sachin-14

--- index.cpp
+++ index.cpp
@@ -2,7 +2,7 @@
 #include <ostream>
 using namespace std;
 
-int main ()   {
-  cout<<"sachin"<<endl;
-    return 0;
+int main() {
+  cout << "sachin" << endl;
+  return 0;
 }
--- init.lua
+++ init.lua
@@ -1,25 +1,22 @@
-require "user.options"
-require "user.keymaps"
-require "user.plugins"
-require "user.colorscheme"
-require "user.cmp"
-require "user.lsp"
-require "user.telescope"
-require "user.treesitter"
-require "user.autopairs"
-require "user.comment"
-require "user.gitsigns"
-require "user.nvim-tree"
+require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.colorscheme")
+require("user.cmp")
+require("user.lsp")
+require("user.telescope")
+require("user.treesitter")
+require("user.autopairs")
+require("user.comment")
+require("user.gitsigns")
+require("user.nvim-tree")
 --require "user.bufferline"
-require "user.lualine"
+require("user.lualine")
 -- gd on "toggleterm" to open the toggleterm.lua file
-require "user.toggleterm"
--- ctrl + \ -> open terminal 
-require "user.todo-comments"
+require("user.toggleterm")
+-- ctrl + \ -> open terminal
+require("user.todo-comments")
 -- :ColorizerAttactToBuffer
-require "user.nvim-colorizer"
-require "user.hologram"
-require "user.live-server"
-
-
-
+require("user.nvim-colorizer")
+require("user.hologram")
+require("user.live-server")
